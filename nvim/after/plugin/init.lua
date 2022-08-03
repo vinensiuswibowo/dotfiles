6a22ffedb142eb3b19a95912b4ae4113d5d4ee47
tech3definite <tech3@definite.co.id>
2022-08-03 21:18:56 +0700

    check empty buffer show telescope

--- nvim/after/plugin/init.lua
+++ nvim/after/plugin/init.lua
@@ -431,7 +431,12 @@ vim.api.nvim_create_autocmd("VimEnter", {
   pattern = "*",
   nested = true,
   callback = function()
-    require('telescope.builtin').find_files()
+    local buf_is_valid = vim.api.nvim_buf_is_valid
+    local buf_get_name = vim.api.nvim_buf_get_name
+    local name = buf_is_valid(1) and buf_get_name(1) or nil
+    if name == '' or not name then
+      require('telescope.builtin').find_files()
+    end
   end,
 })
 
